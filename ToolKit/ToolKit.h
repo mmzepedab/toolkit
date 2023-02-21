007cda448c34e78f0a0c00be5bb43cdc6ffd6d16
Mario Zepeda <mario.zepeda@bairesdev.com>
2023-02-20 22:39:47 -0600

    Initial Commit

---
+++ ToolKit/ToolKit.h
@@ -0,0 +1,18 @@
+//
+//  ToolKit.h
+//  ToolKit
+//
+//  Created by Mario  Zepeda on 2/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToolKit.
+FOUNDATION_EXPORT double ToolKitVersionNumber;
+
+//! Project version string for ToolKit.
+FOUNDATION_EXPORT const unsigned char ToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToolKit/PublicHeader.h>
+
+
