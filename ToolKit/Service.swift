1baf049691335f76c02a0b2e77b2cacb062e8cf8
Mario Zepeda <mario.zepeda@bairesdev.com>
2023-02-24 16:10:47 -0600

    Added podspec

---
+++ ToolKit/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  ToolKit
+//
+//  Created by Mario  Zepeda on 2/20/23.
+//
+
+import Foundation
+
+public struct Service {
+    public static func sayHelloWorld() -> String {
+        return "Hola Mundo"
+    }
+}
